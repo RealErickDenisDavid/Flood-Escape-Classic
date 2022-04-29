9f954830fe52997f3eaecd6f0e71bd27d0d03a6e
PainNonsense <98066737+RealPainNonsense@users.noreply.github.com>
2022-04-29 21:01:45 +0800

    Add files via upload

---
+++ Flood_Escape_Classic.lua
@@ -0,0 +1,35 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+            local Window = Library.CreateLib("Flood Escape Classic", "DarkTheme")
+            
+            -- Main
+
+            local Main = Window:NewTab("Main")
+            local MainSection = Main:NewSection("Main")
+
+            local InfiniteJumpEnabled;
+            MainSection:NewToggle("Infinite Jump", "By toggling this you can jump infinitely", function(State)
+                InfiniteJumpEnabled = State
+
+                local Player = game:GetService("Players").LocalPlayer
+                game:GetService("UserInputService").JumpRequest:Connect(function()
+                    if InfiniteJumpEnabled then
+                        Player.Character:WaitForChild("Humanoid"):ChangeState("Jumping")
+                    end
+                end)
+            end)
+
+            -- Settings
+
+            local Settings = Window:NewTab("Settings")
+            SettingsSelection = Settings:NewSection("Settings")
+        
+            SettingsSelection:NewKeybind("KeybindText", "Change the Keybind", Enum.KeyCode.V, function()
+                Library:ToggleUI()
+            end)
+
+        -- Credits
+
+        local Credits = Window:NewTab("Credits")
+        CreditsSection = Credits:NewSection("Credits")
+
+        CreditsSection:NewLabel("Made by Real_PainNonsense & PainExploit Team#")
